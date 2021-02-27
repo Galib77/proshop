f75c0b193b849762d2555be8630d28eb431e2ea7
Galib77 <razagalib7@gmail.com>
2021-02-27 11:09:06 +0530

    orders detail, added paypal payment, user order details implemented

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,7 @@ import {
     productDetailsReducer,
 } from './reducers/productReducers'
 import {
-    cartReducer
+    cartReducer,
 } from './reducers/cartReducers'
 
 import {
@@ -17,6 +17,10 @@ import {
 } 
   from './reducers/userReducers'
 
+import {
+  orderCreateReducer
+} from './reducers/orderReducers'
+
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
@@ -24,7 +28,8 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -35,9 +40,14 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {}  
+
 const initialState = {
     cart: {
       cartItems: cartItemsFromStorage,
+      shippingAddress: shippingAddressFromStorage,
     },
     userLogin: { userInfo: userInfoFromStorage },
   }
