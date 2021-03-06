210ee7741791197cced9b1ae121606dfe957b523
Galib77 <razagalib7@gmail.com>
2021-03-06 09:37:54 +0530

    checkout process, admin panel, product review and search added

--- frontend/src/components/FormContainer.js
+++ frontend/src/components/FormContainer.js
@@ -2,17 +2,15 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const FormContainer = ({ children }) => {
-    return (
-        <div>
-            <Container>
-                <Row className='justify-content-md-center'>
-                <Col xs={12} md={6}>
-                    {children}
-                </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+  return (
+    <Container>
+      <Row className='justify-content-md-center'>
+        <Col xs={12} md={6}>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default FormContainer
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,34 +2,55 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {
-    productListReducer,
-    productDetailsReducer,
+  productListReducer,
+  productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
+  productReviewCreateReducer,
+  productTopRatedReducer,
 } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 import {
-    cartReducer,
-} from './reducers/cartReducers'
-
-import {
-  userLoginReducer, 
-  userRegisterReducer, 
+  userLoginReducer,
+  userRegisterReducer,
   userDetailsReducer,
-  userUpdateProfileReducer
-} 
-  from './reducers/userReducers'
-
+  userUpdateProfileReducer,
+  userListReducer,
+  userDeleteReducer,
+  userUpdateReducer,
+} from './reducers/userReducers'
 import {
-  orderCreateReducer
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+  orderDeliverReducer,
+  orderListMyReducer,
+  orderListReducer,
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-    cart: cartReducer,
-    userLogin: userLoginReducer,
-    userRegister: userRegisterReducer,
-    userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer,
-    orderCreate: orderCreateReducer
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
+  productTopRated: productTopRatedReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderDeliver: orderDeliverReducer,
+  orderListMy: orderListMyReducer,
+  orderList: orderListReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -42,22 +63,22 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
 
 const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
   ? JSON.parse(localStorage.getItem('shippingAddress'))
-  : {}  
+  : {}
 
 const initialState = {
-    cart: {
-      cartItems: cartItemsFromStorage,
-      shippingAddress: shippingAddressFromStorage,
-    },
-    userLogin: { userInfo: userInfoFromStorage },
-  }
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage },
+}
 
 const middleware = [thunk]
 
 const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-  )
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store
